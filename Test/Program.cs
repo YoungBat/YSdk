34be6dda59b2e1e28029682f51121f36d8debdb5
YBTsa <13703503153@163.com>
2025-04-19 10:55:25 +0800

    Commit

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using YSdk;
+
+namespace Test;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            var cmd = EasyCommand.Parse("download /url fgtf /savePath 123");
+            Console.WriteLine($"{cmd.Command} {cmd["url"]} {cmd["svePath"]}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
