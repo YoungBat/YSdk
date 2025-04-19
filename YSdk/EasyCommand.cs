f67779548dc720f2c44a44bef115ad71d3634950
YBTsa <13703503153@163.com>
2025-04-19 10:48:41 +0800

    Commit

---
+++ YSdk/EasyCommand.cs
@@ -0,0 +1,51 @@
+﻿namespace YSdk;
+
+public class EasyCommand
+{
+    public string Command { get; private set; } = "";
+    private Dictionary<string, string> Arguments { get; } = new();
+
+    public string this[string arg] =>
+        Arguments.GetValueOrDefault(arg) ?? throw new KeyNotFoundException("Argument not found");
+
+    public static EasyCommand Parse(string input, string splitChar = " ")
+    {
+        var segments = input.Split([splitChar], StringSplitOptions.RemoveEmptyEntries);
+
+        var commandValue = segments.Length > 0
+            ? segments[0]
+            : throw new ArgumentException("Invalid command");
+
+        var cmd = new EasyCommand
+        {
+            Command = commandValue
+        };
+        string? currentKey = null;
+        var values = new List<string>();
+
+        foreach (var segment in segments.Skip(1))
+        {
+            if (segment.StartsWith("/"))
+            {
+                if (currentKey is not null)
+                {
+                    cmd.Arguments[currentKey] = string.Join(splitChar, values);
+                    values.Clear();
+                }
+
+                currentKey = segment[1..];
+            }
+            else
+            {
+                values.Add(segment);
+            }
+        }
+
+        if (currentKey is not null)
+        {
+            cmd.Arguments[currentKey] = string.Join(splitChar, values);
+        }
+
+        return cmd;
+    }
+}
